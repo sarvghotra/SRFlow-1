c81cd5c208d6eec87794decee9be61516c63def1
sarvghotra <sssarvjeet27@gmail.com>
2021-07-02 04:42:08 +0000

    Selective dataset fixed

--- code/data/LRHR_selective_dataset.py
+++ code/data/LRHR_selective_dataset.py
@@ -1,4 +1,5 @@
 import os
+import time
 import torch.utils.data as data
 from data.LRHR_dataset import LRHRDataset
 
@@ -9,31 +10,44 @@ class LRHRSelectiveDataset(LRHRDataset):
     faces etc.
     '''
     def __init__(self, opt):
-        super(LRHRSelectiveDataset, self).__init__(opt)
+        self.opt = opt
+        self.phase = opt["name"]
+        self.crop_size = opt.get("GT_size", None)
+        self.scale = opt.get("scale")
+        self.random_scale_list = [1]
 
         selection_txt_file = opt['selection_file']
-        print("Pre size: ", len(self.hr_images))
-        self.hr_images = self.select_imgs(selection_txt_file)
-        print("Post size: ", len(self.hr_images))
+        hr_file_path = opt["dataroot_GT"]
+        self.hr_images = self.select_imgs(hr_file_path, selection_txt_file)
+        self.lr_images = None
 
-    def select_imgs(self, selection_txt_file):
+        gpu = True
+        self.augment = opt["augment"] if "augment" in opt.keys() else False
+        self.use_crop = opt["use_crop"] if "use_crop" in opt.keys() else False
+        self.center_crop_hr_size = opt.get("center_crop_hr_size", None)
+        t = time.time()
+
+
+        t = time.time() - t
+        print("{} Loaded {} HR images".format(self.phase, len(self.hr_images)))
+
+        self.gpu = gpu
+        self.rand_gauss_var = opt["gauss_noise_var"]
+        print("rand gauss var: ", self.rand_gauss_var)
+
+        self.measures = None
+        self.i = 0
+
+    def select_imgs(self, hr_dir_path, selection_txt_file):
         '''
         Filters out images that are not given in the selection_txt_file
         '''
-        selected_filenames = []
+        selected_files = []
         # read the filenames
         with open(selection_txt_file, 'r', encoding='utf-8') as fi:
             for line in fi:
                 line = line.strip()
                 filename = line.split('\t')[0]
-                selected_filenames.append(filename)
+                selected_files.append(os.path.join(hr_dir_path, filename))
 
-        selected_filenames = set(selected_filenames)
-        selected_hr_imgs = []
-
-        for img_path in self.hr_images:
-            filename = os.path.basename(img_path)
-            if filename in selected_filenames:
-                selected_hr_imgs.append(img_path)
-
-        return selected_hr_imgs
+        return selected_files
